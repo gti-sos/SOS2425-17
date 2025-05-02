8b8c03c3edbfc22171572f2617375f8fcd58fb06
Pablo Aguza Barragán <pabagubar1@alum.us.es>
2025-05-02 15:22:47 +0200

    letras

--- src/Frontend/src/routes/Header.svelte
+++ src/Frontend/src/routes/Header.svelte
@@ -203,7 +203,7 @@
             <a href="/university-demands/vistas">Vista university-demands</a>
             <a href="/students_satisfaction/vistas">Vista students-satisfaction</a>
             <a href="/university-academic-performance/vistas">Vista university-academic-performance</a>
-            <a href="/vistas">Vista común</a>
+            <a href="/analytics">Grafica común</a>
 
         </div>
     {/if}
