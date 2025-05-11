b1d19cd101ceff66e96664c5ec9405eb4557ce81
Pablo Aguza Barragán <pabagubar1@alum.us.es>
2025-05-11 20:48:33 +0200

    tamos

--- src/Frontend/src/routes/about/+page.svelte
+++ src/Frontend/src/routes/about/+page.svelte
@@ -87,9 +87,9 @@
         <h4 class="margen_nombres">Alejandro Gaona Penco</h4>
 
         <div style="display: flex; justify-content: center; margin-top: 20px;">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/N7zDqjuHm-4?si=G25hKeIL61Xuwpe7" 
-            title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
-            gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/4b8N3ZqREZg?si=iduzkg775NoQQ2UM" 
+            title="YouTube video player" frameborder="0" allow="accelerometer; autoplay;
+             clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
 
         <style>
